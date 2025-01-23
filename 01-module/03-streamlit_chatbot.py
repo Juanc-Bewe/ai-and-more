04f5d4a32e5aefae96f0881ec22c397f9c63ee9d
JuanCJC <juan.ibanez@bewe.io>
2025-01-23 11:16:13 -0500

    Adding llama call

--- 01-module/03-streamlit_chatbot.py
+++ 01-module/03-streamlit_chatbot.py
@@ -1,14 +1,19 @@
 from langchain_google_vertexai import ChatVertexAI
+from langchain_ollama import ChatOllama
 from helpers.streamlit_chat_helper import streamlit_chat_helper
 from helpers.models_name import VERTEXAI_MODELS
 from langchain.schema import HumanMessage
 
-def chat_with_llm(user_message):
+def chat_with_vertexai(user_message):
     llm = ChatVertexAI(model_name=VERTEXAI_MODELS.GEMINI_1_5_FLASH)
     response = llm.invoke([HumanMessage(content=user_message)])
     return response.content
 
+def chat_with_ollama(user_message):
+    llm = ChatOllama(model="llama3.2")
+    response = llm.invoke([HumanMessage(content=user_message)])
+    return response.content
 
 if __name__ == "__main__":
-    streamlit_chat_helper(chat_with_llm)
+    streamlit_chat_helper(chat_with_ollama)
 
